da3fcf8ac65173498df12311eddb92d467904814
JayadurgaSelvaraj <jayadurga.s2005@gmail.com>
2025-08-13 13:26:55 +0530

    temperature.cs

---
+++ temperature.cs
@@ -0,0 +1,17 @@
+using System;
+class Program
+{
+    static void Main()
+    {
+        float temperature = 36.6f;
+
+        if (temperature > 37.0f)
+        {
+            Console.WriteLine("Fever");
+        }
+        else
+        {
+            Console.WriteLine("Normal");
+        }
+    }
+}
