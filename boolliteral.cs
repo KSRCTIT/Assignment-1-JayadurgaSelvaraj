8d81e03c9e347d338fd0fa4a11a55c4d756ed3b9
JayadurgaSelvaraj <jayadurga.s2005@gmail.com>
2025-08-13 14:27:08 +0530

    boolliteral.cs

---
+++ boolliteral.cs
@@ -0,0 +1,17 @@
+using System;
+
+class Program
+{
+    static void Main()
+    {
+        bool loginSuccess = true;
+        if (loginSuccess)
+        {
+            Console.WriteLine("Login successful");
+        }
+        else
+        {
+            Console.WriteLine("Access denied");
+        }
+    }
+}
